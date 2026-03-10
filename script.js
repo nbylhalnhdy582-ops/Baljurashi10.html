54c60d618e6dbe08de46a4e9779c44b2959e4c2a
nbylhalnhdy582-ops <nbylhalnhdy582@gmail.com>
2026-03-10 06:18:16 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,79 @@
+const welcomeScreen = document.getElementById('welcomeScreen');
+const startBtn = document.getElementById('startBtn');
+const mainContent = document.getElementById('mainContent');
+
+startBtn.addEventListener('click', () => {
+
+  /* اخفاء شاشة الترحيب بهدوء */
+  welcomeScreen.style.opacity = "0";
+
+  setTimeout(() => {
+
+    welcomeScreen.style.display = "none";
+
+    /* شاشة اسم بلجرشي */
+    const intro = document.createElement("div");
+
+    intro.style.position = "fixed";
+    intro.style.top = "0";
+    intro.style.left = "0";
+    intro.style.width = "100%";
+    intro.style.height = "100%";
+    intro.style.background = "rgba(215,155,109,0.85)";
+    intro.style.display = "flex";
+    intro.style.justifyContent = "center";
+    intro.style.alignItems = "center";
+    intro.style.fontSize = "60px";
+    intro.style.color = "darkgreen";
+    intro.style.fontWeight = "bold";
+    intro.style.opacity = "0";
+    intro.style.transition = "opacity 2s";
+
+    intro.innerHTML = "بلجرشي";
+
+    document.body.appendChild(intro);
+
+    setTimeout(() => {
+      intro.style.opacity = "1";
+    }, 100);
+
+    /* بعدها تظهر الصفحة الرئيسية */
+
+    setTimeout(() => {
+
+      intro.style.opacity = "0";
+
+      setTimeout(() => {
+
+        intro.remove();
+
+        mainContent.style.display = "block";
+        mainContent.style.opacity = "0";
+        mainContent.style.transition = "opacity 2s";
+
+        setTimeout(()=>{
+          mainContent.style.opacity = "1";
+        },100);
+
+      },1500);
+
+    },2000);
+
+  },800);
+
+});
+
+
+/* قائمة الهمبرقر */
+
+function toggleMenu(){
+
+  const menu = document.getElementById('menu');
+
+  if(menu.style.display === "block"){
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+
+}
